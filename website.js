2e0edb16e9265d296cb6917923598c2f500bd5c5
ShreshthTiwari <shreshth.shreya24208@gmail.com>
2021-09-06 19:26:18 +0530

    e

--- website.js
+++ website.js
@@ -1,11 +1,19 @@
+
 const http = require("http");
+const fs = require("fs");
 
-module.exports = () =>{
+const PORT = 25575;
+
+module.exports = (command) =>{
+  fs.readFile('./website/index.html', function (err, html) {
+    if (err) throw err;    
   
-  http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');  
-  }).listen(25575);
-
-  console.log('Server running at http://194.163.171.228:25575/');
+    var webServer = http.createServer(function(request, response) {  
+      response.writeHeader(200, {"Content-Type": "text/html"});  
+      response.write(html);  
+      response.end();  
+    });
+    webServer.listen(PORT)
+  });
+  console.log(`\nWebsite running on Port: ${PORT}`);
 }
