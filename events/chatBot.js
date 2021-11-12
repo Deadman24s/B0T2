65b426aecd9f9a624c27cfd23bfa305577f3949d
ShreshthTiwari <shreshth.shreya24208@gmail.com>
2021-11-12 15:33:54 +0530

    Update chatBot.js

--- events/chatBot.js
+++ events/chatBot.js
@@ -6,11 +6,16 @@ module.exports = async(Discord, client, message, args, database, messageEmojiFin
     let chatBotChannel = message.guild.channels.cache.get(chatBotChannelID);
     if(chatBotChannel){
       if(message.channel.id == chatBotChannelID){
+        message.channel.startTyping();
         let input = message.content;
+        if(!input){
+          return;
+        }
         if(input.length > 500){
           input.length= 500;
         }
         let reply = await chatBot.chat({ Message: input });
+        message.channel.stopTyping();
         await message.reply(reply).catch(error => {});
       }
     }
